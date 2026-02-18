f70f8fa6abf701bfa4e524c18b91d276945e35aa
asierputowski <asierputowski@CTMS-LT-0120A>
2026-02-18 14:02:34 -0500

    Updated Direct Messages and Update to the name with U

--- main.go
+++ main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
+	"os"
+
 	"github.com/rivo/tview"
 	"github.com/sirupsen/logrus"
 )
 
-func main(){
+func main() {
 	app := tview.NewApplication()
 	logger := logrus.New()
+	logger.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+	logger.SetLevel(logrus.DebugLevel)
+
+	logFile := "teams-cli.log"
+	f, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o644)
+	if err == nil {
+		logger.SetOutput(f)
+	}
+	logger.WithFields(logrus.Fields{
+		"log_file": logFile,
+		"pid":      os.Getpid(),
+	}).Info("teams-cli starting")
 
 	state := AppState{
-		app: app,
+		app:    app,
 		logger: logger,
 	}
 
 	state.createApp()
-	if err := app.EnableMouse(true).Run(); err != nil {
-		panic(err)
+	if err = app.EnableMouse(true).Run(); err != nil {
+		logger.WithError(err).Fatal("application exited with error")
 	}
 }
--- state_teams.go
+++ state_teams.go
@@ -5,11 +5,13 @@ import (
 	teams_api "github.com/fossteams/teams-api"
 	"github.com/fossteams/teams-api/pkg/csa"
 	"github.com/fossteams/teams-api/pkg/models"
+	"github.com/sirupsen/logrus"
 	"sort"
 )
 
 type TeamsState struct {
 	teamsClient *teams_api.TeamsClient
+	logger      *logrus.Logger
 
 	conversations  *csa.ConversationResponse
 	me             *models.User
@@ -27,21 +29,41 @@ func (s *TeamsState) init(client *teams_api.TeamsClient) error {
 	if client == nil {
 		return fmt.Errorf("client is nil")
 	}
+	if s.logger != nil {
+		s.logger.Debug("teams state initialization started")
+	}
 
 	var err error
 	s.me, err = client.GetMe()
 	if err != nil {
 		return fmt.Errorf("unable to get your profile: %v", err)
 	}
+	if s.logger != nil {
+		s.logger.WithFields(logrus.Fields{
+			"user_display_name": s.me.DisplayName,
+			"user_oid":          s.me.ObjectId,
+			"user_upn":          s.me.UserPrincipalName,
+		}).Info("loaded current user profile")
+	}
 
 	s.pinnedChannels, err = client.GetPinnedChannels()
 	if err != nil {
 		return fmt.Errorf("unable to get pinned channels: %v", err)
 	}
+	if s.logger != nil {
+		s.logger.WithField("pinned_channels_count", len(s.pinnedChannels)).Debug("loaded pinned channels")
+	}
 
 	s.conversations, err = client.GetConversations()
 	if err != nil {
 		return fmt.Errorf("unable to get conversations: %v", err)
+	}
+	if s.logger != nil {
+		s.logger.WithFields(logrus.Fields{
+			"teams_count":         len(s.conversations.Teams),
+			"chats_count":         len(s.conversations.Chats),
+			"private_feeds_count": len(s.conversations.PrivateFeeds),
+		}).Info("loaded conversations payload")
 	}
 
 	// Sort Teams by Name
@@ -59,6 +81,12 @@ func (s *TeamsState) init(client *teams_api.TeamsClient) error {
 				parent:  &t,
 			}
 		}
+	}
+	if s.logger != nil {
+		s.logger.WithFields(logrus.Fields{
+			"team_map_count":    len(s.teamById),
+			"channel_map_count": len(s.channelById),
+		}).Debug("conversation indexes built")
 	}
 
 	return nil
